9b518116803f70790bb53242bd022df7af885fb4
Btn <104200659+Btin7@users.noreply.github.com>
2025-01-09 09:55:59 +0500

    Update datacleaner.py

--- datacleaner/datacleaner.py
+++ datacleaner/datacleaner.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 df = pd.read_csv('emails.csv')
 
-df['text'] = df['text'].str.replace(r'^(Subject:|Re:)\s*', '', regex=True)
+df['text'] = df['text'].str.replace(r'^(Subject:| re:)\s*', '', regex=True)
 
 df.to_csv('emails_cleaned.csv', index=False)
